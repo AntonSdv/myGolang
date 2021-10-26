7218c8a2fd9b27b88c208ccae023f6ea0bdfd685
saidakov_a <Aligator3>
2021-10-26 17:36:41 +0300

    go to web

--- Waether/main.go
+++ Waether/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -16,26 +17,40 @@ type Response struct {
 	MainTemp `json:"main"`
 }
 
-func main() {
-	MakeRequest()
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func MakeRequest() {
+func homeHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("home.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func getWeatherHandler(writer http.ResponseWriter, request *http.Request) {
+	city := request.FormValue("city")
 	var ans Response
-	var city string
-	fmt.Println("Введите город")
-	fmt.Scan(&city)
 	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=49a76d2465245d1581a522d89f08d632&units=metric&lang=ru")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	check(err)
 	body, _ := ioutil.ReadAll(resp.Body)
 	status := resp.Status
 	if status == "404 Not Found" {
-		fmt.Println("Нет такого города")
+		_, err = writer.Write([]byte("Нет такого города"))
+		check(err)
 	} else {
 		err = json.Unmarshal(body, &ans)
-		fmt.Printf("%v\n", ans.MainTemp.Temp)
+		answer := fmt.Sprintf("%f", ans.MainTemp.Temp)
+		_, err = writer.Write([]byte(answer))
+		check(err)
 	}
+}
 
+func main() {
+	http.HandleFunc("/home", homeHandler)
+	http.HandleFunc("/home/weather", getWeatherHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
 }
